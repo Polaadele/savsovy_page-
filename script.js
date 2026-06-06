7292a5dbbcbc3f9630b862172c1d5c11bf54278b
Polaadele <165046057+Polaadele@users.noreply.github.com>
2026-06-06 06:23:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ resetTimer = setTimeout(() => {
     noBtn.style.position = "relative";
     noBtn.style.left = "0";
     noBtn.style.top = "0";
-}, 3000);
+}, 1750);
 
     createFloatingMessage(
         messages[Math.floor(Math.random()*messages.length)],
@@ -74,7 +74,7 @@ function createFloatingMessage(text,x,y){
 
     setTimeout(()=>{
         el.remove();
-    },1500);
+    },3000);
 }
 // للكمبيوتر
 noBtn.addEventListener("mouseenter", moveButton);
