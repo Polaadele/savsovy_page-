85da01ef2cd4ad79f1e2678cf069ac3c624202ba
Polaadele <165046057+Polaadele@users.noreply.github.com>
2026-06-06 05:40:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+const noBtn = document.getElementById("noBtn");
+
+const messages = [
+    "انسي 😒",
+    "لا 😏",
+    "اووف بقى 😂",
+    "مفيش منه ده 😎",
+    "جربي تاني 🤭",
+    "مستحيل 😌",
+    "لا لا لا 😅",
+    "اسااي يعني😅",
+    "ملكيش حل انتي 😍",
+    "خد تعالى هنا 😂",
+    "هيهيه😂",
+
+
+];
+
+const emojis = [
+    "❤️",
+    "🥰",
+    "😂",
+    "💖",
+    "😍",
+    "💕"
+];
+const card = document.getElementById("card")
+let resetTimer;
+
+noBtn.addEventListener("mouseover", () => {
+
+    const maxX = window.innerWidth - noBtn.offsetWidth;
+    const maxY = window.innerHeight - noBtn.offsetHeight;
+
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
+
+    noBtn.style.position = "fixed";
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
+    clearTimeout(resetTimer);
+
+resetTimer = setTimeout(() => {
+    noBtn.style.position = "relative";
+    noBtn.style.left = "0";
+    noBtn.style.top = "0";
+}, 1750);
+
+    createFloatingMessage(
+        messages[Math.floor(Math.random()*messages.length)],
+        x,
+        y
+    );
+
+    createFloatingMessage(
+        emojis[Math.floor(Math.random()*emojis.length)],
+        x + 50,
+        y - 20
+    );
+
+});
+
+function createFloatingMessage(text,x,y){
+
+    const el = document.createElement("div");
+
+    el.className = "floating";
+    el.innerText = text;
+
+    el.style.left = x + "px";
+    el.style.top = y + "px";
+
+    document.body.appendChild(el);
+
+    setTimeout(()=>{
+        el.remove();
+    },1500);
+}
+
+function goNext(){
+    window.location.href = "success.html";
+}
